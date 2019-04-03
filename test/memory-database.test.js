482e66383996a3523867cfd3598063c5b2cbb516
Masmics <cherlocker3113@gmail.com>
2019-04-03 10:03:25 -0700

    working on find function

--- test/memory-database.test.js
+++ test/memory-database.test.js
@@ -2,9 +2,11 @@ const MemoryDatabase = require('../lib/memory-database');
 
 describe('memory-db methods', () => {
   let db;
+
   beforeEach(() => {
     db = new MemoryDatabase();
   });
+
   describe('memory database (create method)', () => {
     it('checks store is instantiated', () => {
       expect(db.store).toEqual({});
@@ -16,11 +18,24 @@ describe('memory-db methods', () => {
       const newDog = db.create(dog);
       expect(Object.values(db.store)).toContainEqual(newDog);
     });
-    // it('can create unique uuid', () => {
-    //   const dog = {
-    //     name: 'rufus',
-    //     _id: ''
-    //   };
-    // });
+  });
+  
+
+
+  // describe('findById method', () => {
+  //   it('can find object  by ID', () => {
+  //     const dog = {
+  //       name: 'rufus'
+  //     };
+  //     const newDog = db.create(dog);
+//
+//newDog should hold the returned object with the id in it;
+//from there, bracket notate the id off the store
+
+  //     expect(db.findById(newDog.id)).toEqual(newDog);
+  //   });
+
   });
 });
+
+//findById
