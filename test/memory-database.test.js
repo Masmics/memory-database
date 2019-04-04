0ac71e4ef802ca6b083b531b442eb2b0ac74f1b1
Masmics <cherlocker3113@gmail.com>
2019-04-03 21:51:18 -0700

    findById added

--- test/memory-database.test.js
+++ test/memory-database.test.js
@@ -6,7 +6,6 @@ describe('memory-db methods', () => {
   beforeEach(() => {
     db = new MemoryDatabase();
   });
-
   describe('memory database (create method)', () => {
     it('checks store is instantiated', () => {
       expect(db.store).toEqual({});
@@ -19,4 +18,13 @@ describe('memory-db methods', () => {
       expect(Object.values(db.store)).toContainEqual(newDog);
     });
   });
+  describe('findById method', () => {
+    it('can find an obj by id in the db', () => {
+      const dog = { name: 'rufus' };
+      const newDog = db.create(dog);
+      const findDog = db.findById(newDog.id);
+      expect(findDog).toEqual(newDog);
+    });
+  });
+
 });
